b299b2f25b6487000c5787a29d63fc53a4d1bfe9
Koós Zoltán <koos.zoltan.iso@gmail.com>
2025-03-19 09:56:17 +0100

    Moved VatRegistrationRequest from VatRegistrationController

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VatRegistrationRequest.cs
@@ -0,0 +1,8 @@
+public class VatRegistrationRequest
+    {
+        public string CompanyName { get; set; }
+
+        public string CompanyId { get; set; }
+
+        public string Country { get; set; }
+    }
