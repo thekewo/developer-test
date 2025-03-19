6aba0e3bc6791e9ddb51dc70ddcbdc7685439cf3
Koós Zoltán <koos.zoltan.iso@gmail.com>
2025-03-19 09:58:47 +0100

    Updated service setup

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -1,11 +1,36 @@
+using System.Globalization;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Taxually.TechnicalTest;
+using Taxually.TechnicalTest.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("en");
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<VatRegistrationRequestValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register HTTP client for UK API
+builder.Services.AddSingleton<ITaxuallyHttpClient, TaxuallyHttpClient>();
+// Register queue clients for CSV and XML uploads
+builder.Services.AddSingleton<ITaxuallyQueueClient, TaxuallyQueueClient>();
+// Register strategies
+builder.Services.AddScoped<IVatRegistrationStrategy, UKVatRegistrationStrategy>();
+builder.Services.AddScoped<IVatRegistrationStrategy, FranceVatRegistrationStrategy>();
+builder.Services.AddScoped<IVatRegistrationStrategy, GermanyVatRegistrationStrategy>();
+
+// Register strategy factory
+builder.Services.AddScoped<IVatRegistrationStrategyFactory, VatRegistrationStrategyFactory>();
+
+// Register logging
+builder.Services.AddLogging();
+        
 
 var app = builder.Build();
 
